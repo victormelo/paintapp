89a140d4b4b23ac60156cf20526659ba1f6e2584
victormelo <victorklbr@gmail.com>
2015-03-26 02:59:37 -0300

    on touch draw an ellipse, on move draw a line

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse, Line
+from random import random
 
 
 class PaintWidget(Widget):
-    pass
+    def on_touch_down(self, touch):
+
+        with self.canvas:
+            Color(random(), random(), random())
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
 class MyPaintApp(App):
